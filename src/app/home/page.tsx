92ac811fa94dd4f855dfc870118b322b403dc307
mahmud006 <mh.mahmud006@gmail.com>
2025-08-03 00:24:58 +0600

    feat: img switched to svg for better quality

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <HeroSection />
       <div className='sm:hidden absolute top-[76px] left-1/2 -translate-x-1/2 z-0'>
         <Image
-          src='/assets/mobile-mockup-xs.webp'
+          src='/assets/mobile-mockup-xs.svg'
           alt='Fametonic Mobile App'
           width={390}
           height={426}
@@ -35,7 +35,7 @@ export default function Home() {
       </div>
       <div className='hidden sm:block absolute top-[156px] left-1/2 -translate-x-1/2 lg:left-auto lg:translate-x-0 lg:top-[126px] lg:right-[20px] xl:right-[60px] 2xl:right-[101px]'>
         <Image
-          src='/assets/mobile-mockup.webp'
+          src='/assets/mobile-mockup.svg'
           alt='Fametonic Mobile App'
           width={666}
           height={679}
