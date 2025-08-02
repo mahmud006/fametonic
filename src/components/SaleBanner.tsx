0e1ae912d7a0a1424d3574eefbc3d57dc142b870
mahmud006 <mh.mahmud006@gmail.com>
2025-08-02 23:45:45 +0600

    feat: png img incorporated, banner size issue fixed

--- src/components/SaleBanner.tsx
+++ src/components/SaleBanner.tsx
@@ -6,22 +6,22 @@ export default function SaleBanner() {
   ];
   return (
     <div
-      className='h-[76px] lg:h-[46px] px-5 md:px-[20px] text-center flex items-center justify-center md:w-full mx-auto'
+      className='h-[76px] lg:h-[46px] px-0 xs:px-5 md:px-[20px] text-center flex items-center justify-center md:w-full mx-auto'
       style={{
         background:
           'linear-gradient(89.63deg, #FC004E -2.16%, #10CBE0 115.27%)',
       }}
     >
       <div className='flex flex-col items-center gap-1 sm:hidden'>
-        <div className='flex flex-row items-center gap-1'>
-          <span className='text-[#10CBE0] font-extrabold text-[16px] leading-[100%] text-center'>
+        <div className='flex flex-row gap-1 items-center justify-center whitespace-nowrap'>
+          <span className='text-[#10CBE0] font-extrabold text-[14px] min-[390px]:text-[16px] leading-[100%] text-center'>
             {textParts[0].text}
           </span>
-          <span className='text-white font-extrabold text-[16px] leading-[100%] text-center'>
+          <span className='text-white font-extrabold text-[14px] min-[390px]:text-[16px] leading-[100%] text-center'>
             {textParts[1].text}
           </span>
         </div>
-        <span className='text-white font-semibold text-[16px] leading-[100%] text-center'>
+        <span className='text-white font-semibold text-[14px] min-[390px]:text-[16px] leading-[100%] text-center px-2'>
           {textParts[2].text}
         </span>
       </div>
